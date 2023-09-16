ad1a3abad635d3edb12307f4ebfe8b437b84aa33
Shashi Raj <shashi.raj30@yahoo.in>
2023-09-16 11:25:34 +0000

    Add changes

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 sudo apt-get update && sudo apt-get install ffmpeg libsm6 libxext6  -y
 sudo apt-get install libapache2-mod-wsgi python-dev -y
+wget https://repo.anaconda.com/archive/Anaconda3-2023.07-2-Linux-x86_64.sh
+bash Anaconda3-2023.07-2-Linux-x86_64.sh
 pip install -r requirements.txt
